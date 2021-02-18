c57af0f631ccf7527f4702fc545457141f56be01
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-02-18 17:18:19 +0000

    renderNewEntry in render module now working

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@
 
 import loadPage from "./modules/loadPage";
 import addToStorage from "./modules/createEntry";
-import { renderStorage } from "./modules/renders";
+import { renderStorage, renderEntry } from "./modules/renders";
 
 loadPage();
 renderStorage();
 
 const addProjectBtn = document.getElementById("add-project-btn");
-addProjectBtn.addEventListener("click", addToStorage.project);
+addProjectBtn.addEventListener("click", () => {
+	addToStorage.project();
+	renderEntry.project();
+});
 
 const addListBtn = document.getElementById("add-list-btn");
-addListBtn.addEventListener("click", addToStorage.list);
+addListBtn.addEventListener("click", () => {
+	addToStorage.list();
+	renderEntry.list();
+});
 
 const addTaskBtn = document.getElementById("add-task-btn");
-addTaskBtn.addEventListener("click", addToStorage.task);
+addTaskBtn.addEventListener("click", () => {
+	addToStorage.task();
+	renderEntry.task();
+});
--- src/modules/renders.js
+++ src/modules/renders.js
@@ -2,65 +2,107 @@
 
 import storage from "./storage";
 
+const displayProject = (project) => {
+	const projectDiv = document.createElement("div");
+	const sidebar = document.getElementById("sidebar");
+	const text = project.projectName;
+
+	projectDiv.setAttribute("class", "project");
+	projectDiv.setAttribute("id", text);
+	projectDiv.textContent = text;
+
+	sidebar.appendChild(projectDiv);
+};
+
+const displayList = (list, project) => {
+	const listDiv = document.createElement("div");
+	const text = list.listName;
+	const inProject = document.getElementById(`${project.projectName}`); // add project by active class here...
+
+	listDiv.setAttribute("class", "list");
+	listDiv.setAttribute("in-project", project.projectName);
+	listDiv.textContent = text;
+
+	inProject.appendChild(listDiv);
+};
+
+const displayTask = (task, list) => {
+	const taskDiv = document.createElement("div");
+	const tasksContainer = document.getElementById("tasks-container"); // add list by active class here
+	const text = task.taskName;
+
+	taskDiv.setAttribute("class", "task");
+	taskDiv.setAttribute("in-list", list.listName);
+	taskDiv.textContent = text;
+
+	tasksContainer.appendChild(taskDiv);
+};
+
 const renderStorage = () => {
-	const displayProjects = () => {
+	const displayAllProjects = () => {
 		storage.forEach((project) => {
-			const projectDiv = document.createElement("div");
-			const sidebar = document.getElementById("sidebar");
-			const text = project.projectName;
-
-			projectDiv.setAttribute('class', 'project');
-			projectDiv.setAttribute("id", text);
-			projectDiv.textContent = text;
-
-			sidebar.appendChild(projectDiv);
+			displayProject(project);
 		});
 	};
 
-	const displayLists = () => {
+	const displayAllLists = () => {
 		storage.forEach((project) => {
 			project.lists.forEach((list) => {
-				const listDiv = document.createElement("div");
-				const text = list.listName;
-				const inProject = document.getElementById(`${project.projectName}`);
-
-				listDiv.setAttribute('class', 'list');
-				listDiv.setAttribute('in-project', project.projectName);
-				listDiv.textContent = text;
-
-				inProject.appendChild(listDiv);
+				displayList(list, project);
 			});
 		});
 	};
 
-	// add tasks to the dom
-	// function displayTasks(){
-	const displayTasks = () => {
+	const displayAllTasks = () => {
 		storage.forEach((project) => {
 			project.lists.forEach((list) => {
 				list.tasks.forEach((task) => {
-					const taskDiv = document.createElement('div');
-					const tasksContainer = document.getElementById('tasks-container');
-					const text = task.taskName;
-
-					taskDiv.setAttribute('class', 'task');
-					taskDiv.setAttribute('in-list', list.listName);
-					taskDiv.textContent = text;
-
-					tasksContainer.appendChild(taskDiv);
+					displayTask(task, list);
 				});
 			});
 		});
 	};
 
-	
-	displayProjects();
-	displayLists();
-	displayTasks();
+	displayAllProjects();
+	displayAllLists();
+	displayAllTasks();
 };
 
 // funtion that render new entrys
-const renderNewEntry = () => {
+const renderEntry = (() => {
+	const project = () => {
+		storage.forEach((project) => {
+			const newProject = document.getElementById("sidebar-input").value;
+			if (project.projectName === newProject) {
+				displayProject(project);
+			}
+		});
+	};
+	const list = () => {
+		storage.forEach((project) => {
+			project.lists.forEach((list) => {
+				const newList = document.getElementById("sidebar-input").value;
+				if (list.listName === newList) {
+					displayList(list, project);
+				}
+			});
+		});
+	};
 
-}
-export { renderStorage, renderNewEntry };
+	const task = () => {
+		storage.forEach((project) => {
+			project.lists.forEach((list) => {
+				list.tasks.forEach((task) => {
+					const newTask = document.getElementById("task-input").value;
+					if ((task.taskName === newTask)) {
+						displayTask(task, list);
+					}
+				});
+			});
+		});
+	};
+
+	return { project, list, task };
+})();
+
+export { renderStorage, renderEntry };
